f15606b9e8dd4febb82ee564721cc20880a77b0b
RenzMc <155152681+RenzMc@users.noreply.github.com>
2024-10-04 21:31:03 +0700

    add support for acacia Wood and all kinds of wood

--- src/Renz/FallingTree/Main.php
+++ src/Renz/FallingTree/Main.php
@@ -5,10 +5,10 @@ namespace Renz\FallingTree;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\block\VanillaBlocks;
+use pocketmine\block\Wood;
 use pocketmine\block\Block;
-use pocketmine\math\Vector3;
 use pocketmine\player\Player;
+use pocketmine\math\Facing;
 
 class Main extends PluginBase implements Listener {
 
@@ -20,7 +20,7 @@ class Main extends PluginBase implements Listener {
         $player = $event->getPlayer();
         $block = $event->getBlock();
 
-        if ($this->hasFallingTreePermission($player) && $this->isLog($block)) {
+        if ($this->hasFallingTreePermission($player) && $block instanceof Wood) {
             $this->makeTreeFall($block);
         }
     }
@@ -29,38 +29,13 @@ class Main extends PluginBase implements Listener {
         return $player->hasPermission("fallingtree.use");
     }
 
-    private function isLog(Block $block): bool {
-        $logBlocks = [
-            VanillaBlocks::OAK_LOG(),
-            VanillaBlocks::SPRUCE_LOG(),
-            VanillaBlocks::BIRCH_LOG(),
-            VanillaBlocks::JUNGLE_LOG(),
-            VanillaBlocks::ACACIA_LOG(),
-            VanillaBlocks::DARK_OAK_LOG()
-        ];
-
-        foreach ($logBlocks as $logBlock) {
-            if ($block->getTypeId() === $logBlock->getTypeId()) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     private function makeTreeFall(Block $block): void {
-        $world = $block->getPosition()->getWorld();
-        $x = $block->getPosition()->getX();
-        $y = $block->getPosition()->getY();
-        $z = $block->getPosition()->getZ();
-
-        for ($i = $y + 1; $i < $world->getMaxY(); $i++) {
-            $currentBlock = $world->getBlockAt($x, $i, $z);
-            if ($this->isLog($currentBlock)) {
-                $world->setBlockAt($x, $i, $z, VanillaBlocks::AIR());
-                $world->dropItem(new Vector3($x, $i, $z), $currentBlock->asItem());
-            } else {
-                break;
+        $blockPos = $block->getPosition();
+        foreach (Facing::ALL as $face) {
+            $sideBlock = $block->getSide($face);
+            if ($sideBlock instanceof Wood && $block->hasSameTypeId($sideBlock)) {
+                $blockPos->getWorld()->useBreakOn($sideBlock->getPosition());
+                $this->makeTreeFall($sideBlock);
             }
         }
     }
